7c6be8b4df20213ac2e157a6292f36acce138ea8
tsudos <tsud@sfr.fr>
2020-01-14 15:12:09 +0100

    install de fixtures et création de 15 personnes de la table Costumer

--- src/DataFixtures/CostumerFixtures.php
+++ src/DataFixtures/CostumerFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Customer;
 
 class CostumerFixtures extends Fixture
 {
@@ -11,6 +12,35 @@ class CostumerFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        for ($i = 1; $i <=15;$i++) {
+            if ( $i%2 != 0 ){
+                $customer = new Customer();
+                $customer->setFirstname("Die".$i);
+                $customer->setLastname($i);
+                $customer->setEmail($i."@Gmail.fr");
+                $customer->setAdress($i." rue des diables 38510 vezeronce");
+                $customer->setPhone("050609070".$i);
+                $customer->setBirthDate(NULL);
+                $customer->setCoastalLicense(NULL);
+                $customer->setReduction(NULL);
+                /*preparer le manager à persister les données*/
+                $manager->persist($customer);
+                }
+            else {
+                $customer = new Customer();
+                $customer->setFirstname("Gret".$i);
+                $customer->setLastname($i);
+                $customer->setEmail($i."@sfr.fr");
+                $customer->setAdress($i." rue des anges 38510 morestel");
+                $customer->setPhone("050609070".$i);
+                $customer->setBirthDate(NULL);
+                $customer->setCoastalLicense("czecs566666".$i);
+                $customer->setReduction(NULL);
+                /*preparer le manager à persister les données*/
+                $manager->persist($customer);
+            }
+        }           
 
         $manager->flush();
     }
