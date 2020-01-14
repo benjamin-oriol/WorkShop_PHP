87a869e21c0be17803fc17d7a988146b8a6659e9
benjamin-oriol <oriol.benjamin@gmail.com>
2020-01-14 16:51:31 +0100

    register login home ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
@@ -51,7 +52,20 @@ class SecurityController extends AbstractController
      * @Route ("/connexion", name="security_login")
      *
      */
-    public function login() {
-        return $this->render('security/login.html.twig');
+    public function login( AuthenticationUtils $authentication) {
+        $error= $authentication->getLastAuthenticationError();
+        $lastusername= $authentication->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error'=>$error,
+            'lastusername'=>$lastusername
+        ]); 
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function logout() {
+
     }
 }
