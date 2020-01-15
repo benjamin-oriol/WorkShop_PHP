fdab3d228c64e07a0a8fe4ca3f258008bab67f0e
tsudos <tsud@sfr.fr>
2020-01-15 16:53:25 +0100

    creation du formulaire et de la page de creation

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -7,6 +7,14 @@ use App\Repository\CustomerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CustomerController extends AbstractController
@@ -36,14 +44,38 @@ class CustomerController extends AbstractController
         $customer = new Customer();
 
         $form = $this->createFormBuilder($customer)
-                        ->add('firstname')
-                        ->add('lastname')
-                        ->add('email')
-                        ->add('adress')
-                        ->add('phone')
-                        ->add('birth_date')
-                        ->add('coastal_license')
-                        ->add('reduction')
+                        ->add('firstname', TextType::class, [
+                            'label' => 'Nom',
+                            'attr' => ['placeholder' => "Nom du client", 'class' => 'form-control']
+                        ])
+                        ->add('lastname', TextType::class, [
+                            'label' => 'Prenom',
+                            'attr' => ['placeholder' => "Prenom du client", 'class' => 'form-control']
+                        ])
+                        ->add('email', EmailType::class, [
+                            'label' => 'Email',
+                            'attr' => ['placeholder' => "Email du client", 'class' => 'form-control']
+                        ])
+                        ->add('adress', TextType::class, [
+                            'label' => 'Adresse',
+                            'attr' => ['placeholder' => "Adresse du client", 'class' => 'form-control']
+                        ])
+                        ->add('phone', TelType::class, [
+                            'label' => 'Telephone',
+                            'attr' => ['placeholder' => "Telephone du client", 'class' => 'form-control']
+                        ])
+                        ->add('birth_date',  BirthdayType::class, [
+                            'label' => 'Date d\'anniversaire',
+                            'attr' => ['class' => 'form-control']
+                            ])
+                        ->add('coastal_license', TextType::class, [
+                            'label' => 'Permis Cotier',
+                            'attr' => ['placeholder' => "Permis Cotier du client", 'class' => 'form-control']
+                        ])
+                        ->add('reduction', NumberType::class, [
+                            'label' => 'reduction',
+                            'attr' => ['class' => 'form-control']
+                            ])
                         ->getForm();
         return $this->render('customer/create.html.twig',['formCustomer' => $form->createView()]);
     }
