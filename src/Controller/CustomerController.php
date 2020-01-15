12987e1f811834aacdbe5cbd22bdf7d78f2cde52
tsudos <tsud@sfr.fr>
2020-01-15 14:42:00 +0100

    ok pour le passge id par le path + modif des dependances

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Repository\CustomerRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,9 +12,10 @@ class CustomerController extends AbstractController
     /**
      * @Route("/customer", name="customer")
      */
-    public function index()
+    public function index(CustomerRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Customer::class);
+        /*grace au parametre CustomerRepository $repo =>
+        $repo = $this->getDoctrine()->getRepository(Customer::class);*/
 
         $customers = $repo->findAll();
         return $this->render('customer/index.html.twig', [
@@ -21,14 +23,16 @@ class CustomerController extends AbstractController
         ]);
     }
 
+
     /**
      * @Route("/customer/{id}", name="customer_change")
      */
-    public function change($id)
+    public function change(Customer $customer/*,$id*/)
     {
+        /*grace au parametre Customer $customer =>
         $repo = $this->getDoctrine()->getRepository(Customer::class);
 
-        $customer = $repo->find($id);
+        $customer = $repo->find($id);*/
     return $this->render('customer/change.html.twig', ['customer' => $customer]);
     }
 }
