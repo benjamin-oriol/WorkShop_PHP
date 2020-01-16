1cc13e57ca4bc572c4135133a7bf166163fbf0c7
Alexis MARECHAL <alexis.marechal@smile.fr>
2020-01-16 10:42:26 +0100

    Change password and params

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,15 +6,19 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController{
-    //Annotations pour la route
+    
     /**
      * @Route("/", name="home")
      */
-
-     public function Home(){
-       return $this->render('main/home.html.twig', [
-           'name' => 'Benjamin'
-       ]);
+     public function home(){
+       return $this->render('main/home.html.twig');
      }
+
+     /**
+     * @Route("/parameters", name="parameters")
+     */
+    public function parameters(){
+      return $this->render('main/parameters.html.twig');
+    }
 }
 ?>
